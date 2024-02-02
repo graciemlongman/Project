679ac4d6f18c3b6d247f5295202e99955f50e4a5
Gracie <119215066+graciemlongman@users.noreply.github.com>
2024-02-02 15:10:58 +0000

    Update extract_energy_only.py

--- extract_energy_only.py
+++ extract_energy_only.py
@@ -1,20 +1,22 @@
 import sys
 import mmap
-from decimal import Decimal
 
 #read in files to extract data from
 file = sys.argv[1]
-Energy = 0
 
 #open each file
 with open(file, "r") as fil:
 	#memory map the file
 	map = mmap.mmap(fil.fileno(), 0, prot=mmap.PROT_READ)
-	#searcg for last occurence of word
+	
+	#search for last occurence of word
 	i = map.rfind(b"E(RB3LYP)")
+	
 	#seek to the location
 	map.seek(i)
+	
 	# read the line
 	line = map.readline()
+	
 	#print to cmd line
 	print(f'file: {file}, Energy: {line[11:30]}')
