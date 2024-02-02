5273de9ee3e0528c081a5034f6104efe90c1f193
Gracie <119215066+graciemlongman@users.noreply.github.com>
2024-02-02 15:03:48 +0000

    Add files via upload

---
+++ extract_results_monomer.py
@@ -0,0 +1,30 @@
+import sys
+import mmap
+from decimal import Decimal
+
+#read in files to extract data from
+file = sys.argv[1]
+#list of specific phrases in log file which correspond to final energy and specific bond lengths: Pd-Cl, Pd-P, Pd-P, Pd-P(donor)
+measurements = [b'E(RB3LYP)', b'R(1,3)', b'R(1,4)', b'R(1,5)', b'R(1,6)']
+
+
+#cmd line formatting
+print(f'{file}')
+
+#open each file
+with open(file, "r") as fil:
+	#memory map the file
+	map = mmap.mmap(fil.fileno(), 0, prot=mmap.PROT_READ)
+	
+	for m in measurements:
+		#search for last occurence of measurement you want to extract
+		i = map.rfind(m)
+		#seek to the location
+		map.seek(i)
+		# read the line
+		result = map.readline()
+		#print to cmd line
+		print(f'{result}')
+	print('------------------------------------------------------------------------')
+
+
