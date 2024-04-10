008c582e30833ac9b460281ad0014230db3ac107
Gracie <119215066+graciemlongman@users.noreply.github.com>
2024-04-10 02:23:45 +0100

    Add files via upload

--- extract_results_monomer.py
+++ extract_results_monomer.py
@@ -1,10 +1,12 @@
 import sys
 import mmap
+from decimal import Decimal
 
 #read in files to extract data from
 file = sys.argv[1]
 #list of specific phrases in log file which correspond to final energy and specific bond lengths: Pd-Cl, Pd-P, Pd-P, Pd-P(donor)
 measurements = [b'E(RB3LYP)', b'R(1,3)', b'R(1,4)', b'R(1,5)', b'R(1,6)']
+
 
 #cmd line formatting
 print(f'{file}')
